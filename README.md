2affbd459c45e2e36237594ed12dcdf36fd73ebc
Brian Guzmán <b.guzman.m@me.com>
2023-03-29 21:42:20 -0300

    basico javascript

---
+++ README.md
@@ -0,0 +1,30 @@
+# Declara una variable en JavaScript
+
+Declaremos e inicializamos en una sola línea
+
+```javascript
+var nombre_variable = <valor_inicial>;
+```
+
+# Tipos de datos primitivos en JavaScript
+
+- Numericos (number)
+- Cadena de caracteres (string)
+- logicos (verdaro o falso, boolean)
+- null (nulo)
+- undefined (no definido)
+
+# Declarar una constante en JavaScript
+
+```javascript
+const nombre_constante = <valor>;
+```
+
+# Operadores JavaScript
+
+- `=` : asignación
+- `+` : sumar dos valores
+- `-` : restar dos valores
+- `*` : multiplicar dos valores
+- `/` : dividir dos valores
+- `%` : mod, obtiene el resto de una división.
---
+++ index.js
@@ -0,0 +1,43 @@
+var x = 10;
+x = 20;
+var a = 1 + 2 * x;
+console.log(a);
+
+var nombre = "Brian";
+nombre = "Brian Guzmán";
+console.log(nombre);
+
+var divorciado = true;
+
+var fechaDefuncion;
+console.log(fechaDefuncion);
+
+const IVA = 19;
+
+console.log(IVA);
+// esto da un error, no se puede cambiar el valor de una constante
+//IVA = 20;
+
+var nota1;
+var nota2;
+var nota3;
+var promedio;
+
+nota1 = 7;
+nota2 = 2.5;
+nota3 = 5;
+
+nota2 = 4.5;
+
+var ponderacion1 = 0.2;
+var ponderacion2 = 0.5;
+var ponderacion3 = 0.3;
+
+promedio = ((nota1 * ponderacion1) + (nota2 * ponderacion2) + (nota3 * ponderacion3)) - 1;
+console.log(promedio);
+
+var numero = 5;
+var divisor = 2;
+
+var resultado = numero % divisor;
+console.log(resultado);
